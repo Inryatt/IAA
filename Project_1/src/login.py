806a584ef54c144262198a45fd7374962fb817cd
rodrigoflima <rodrigoflima@ua.pt>
2023-05-06 11:34:09 +0100

    cleanup

--- Project_1/src/login.py
+++ Project_1/src/login.py
@@ -9,40 +9,30 @@ import sqlite3
 def get_idps():
     DB_LOCATION = "/etc/project_1.db"
 
-    # Check if the database exists
     if not os.path.exists(DB_LOCATION):
         return None
 
-    # Connect to the database
     conn = sqlite3.connect(DB_LOCATION)
     c = conn.cursor()
 
-    # Get the IDPs
     c.execute("SELECT * FROM idps")
     idps = c.fetchall()
 
-    # Close the connection
     conn.close()
 
     return idps
 
 def qr_code(url):
-    # Create a QR code instance
     qr = qrcode.QRCode(version=1, box_size=1, border=1)
 
-    # Add the data to the QR code
     qr.add_data(url)
 
-    # Compile the QR code
     qr.make(fit=True)
 
-    # Get the QR code data as a list of lists
     data = qr.get_matrix()
 
-    # Print the URL
     print("\033[1;33m[?]\033[0m Visit the following URL to authenticate: " + url)
 
-    # Print the QR code to the terminal with colors and spacing
     print("\033[33m[-]\033[0m Or scan the following QR code:")
     for row in data:
         for cell in row:
@@ -54,34 +44,26 @@ def qr_code(url):
 
 def auth(username):
     if username != None:
-        # print with colors
         print("\033[1;32m[+]\033[0m Authenticating user: " + username)
     
-    # Get the IDPs
     idps = get_idps()
 
-    # Check if there are any IDPs
     if idps == None:
         print("\033[1;31m[!]\033[0m No IDPs configured")
-        # print with orange color
         print("\033[33m[-]\033[0m Falling back to local authentication")
         return False
 
-    # Print the menu
     while True:
         print("\033[1;33m[?]\033[0m Select an IDP:")
         for i in range(len(idps)):
             print(str(i) + ") " + idps[i][0])
         print("q) Quit")
 
-        # Get the user's choice
         choice = raw_input("> ")
 
-        # Check if the user wants to quit
         if choice == "q":
             return False
 
-        # Check if the user's choice is valid
         try:
             choice = int(choice)
             if choice < 0 or choice >= len(idps):
@@ -90,10 +72,8 @@ def auth(username):
             print("[-] Invalid choice")
             continue
 
-        # Get the IDP
         idp = idps[choice]
 
-        # Print the IDP's information
         print("\033[1;32m[+]\033[0m IDP: " + idp[0])
         break
 
